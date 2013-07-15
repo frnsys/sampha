d6442675e6b22ba47a7cf305a36117cbfc9d65b5
Francis Tseng <ftzeng@gmail.com>
2013-07-15 11:24:29 -0400

    less crazy dropdown animations

--- js/main.js
+++ js/main.js
@@ -87,9 +87,9 @@
 
     // Release dropdowns
     $('.release').on('mouseenter', function() {
-        $(this).find('ul').slideDown();
+        $(this).find('ul').stop().slideDown();
     }).on('mouseleave', function() {
-        $(this).find('ul').slideUp();
+        $(this).find('ul').stop().slideUp();
     });
 
     // External links pause the track.
